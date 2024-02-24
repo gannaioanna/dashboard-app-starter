bb97e53693176fe200a60a57187dc04e27ba1413
Anna Ignateva <gannaioannovna25@gmail.com>
2024-02-24 23:15:29 +0400

    second commit

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,27 @@
-import data from '../mock/data.json';
+import { useSelector, useDispatch } from 'react-redux';
 import { JobPosition } from './JobPosition';
+import { selectAllPosition } from 'store/positions/position-selectors';
+import { addFilter } from 'store/filters/filter-action';
 
 const JobList = () => {
+  const dispatch = useDispatch();
+  const positions = useSelector(selectAllPosition);
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
+
   return (
     <div className='job-list'>
-      {data.map(item => (
-        <JobPosition key={item.id} {...item} />
+      {positions.map((item) => (
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export {JobList};
+export { JobList };
